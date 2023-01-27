2988f3bd4cff36d0358326a24d70abd1255cfa49
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2023-01-27 08:29:19 +0200

    Make a simple board grid

---
+++ app.js
@@ -0,0 +1,12 @@
+let board = document.querySelector(".board");
+board.style.gridTemplateColumns = "repeat(16, 1fr)";
+board.style.gridTemplateRows = "repeat(16, 1fr)";
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement('div')
+    square.style.backgroundColor = 'blue';
+    // square.style.border = '1px solid black'
+    
+    board.appendChild(square)
+    // board.insertAdjacentElement('beforeend', square)
+}
