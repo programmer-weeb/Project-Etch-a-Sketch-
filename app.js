54c424b3f91ed46a0336c45226785f0915da424c
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2023-01-27 08:49:10 +0200

    Finish board with error message

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+function populateBoard(size) {
+	let board = document.querySelector(".board");
+	board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+	// board.textContent = '';
+	// remove all existing before populate
+	board.innerHTML = "";
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'blue';
-    // square.style.border = '1px solid black'
-    
-    board.appendChild(square)
-    board.insertAdjacentElement('beforeend', square)
+	for (let i = 0; i < size * size; i++) {
+		let square = document.createElement("div");
+		square.style.backgroundColor = "blue";
+		// square.style.border = '1px solid black'
+
+		board.appendChild(square);
+		board.insertAdjacentElement("beforeend", square);
+	}
+}
+populateBoard(16);
+function changeSize(input) {
+	if (input >= 2 || input <= 100) {
+		populateBoard(input);
+	} else {
+        console.log('invalid input');
+    }
 }
