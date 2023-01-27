0fee4241cfd35adc5fd051e530d26dd2b8a8b797
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2023-01-27 10:31:17 +0200

    Add functionality to buttons

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+let color = "black";
+
 function populateBoard(size) {
 	let board = document.querySelector(".board");
+	board.style.border = "1px solid";
 	board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 	board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 	// board.textContent = '';
@@ -8,22 +11,43 @@ function populateBoard(size) {
 
 	for (let i = 0; i < size * size; i++) {
 		let square = document.createElement("div");
-		square.style.backgroundColor = "blue";
-        // change color on hover
-        square.addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor = "black";
-        })
+		square.style.backgroundColor = "white";
+		// change color on hover
+		square.addEventListener("mouseover", changeSquareColor);
 		// square.style.border = '1px solid black'
 
 		board.appendChild(square);
 		board.insertAdjacentElement("beforeend", square);
 	}
 }
+
+function changeSquareColor(e) {
+	// e.target.style.backgroundColor = "black";
+	if (color == "random") {
+        this.style.backgroundColor = 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)'
+	} else {
+		this.style.backgroundColor = color;
+	}
+}
+
 populateBoard(16);
+
 function changeSize(input) {
 	if (input >= 2 || input <= 100) {
 		populateBoard(input);
 	} else {
-        console.log('invalid input');
-    }
+		console.log("invalid input");
+	}
+}
+
+function changeColor(choice) {
+	color = choice;
+}
+
+function reset () {
+    let board = [...document.querySelector(".board")];
+    board.forEach(element => {
+        element.style.backgroundColor = 'white'
+    });
+    
 }
